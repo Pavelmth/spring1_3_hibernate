f40cf0353c4199dacd3f22675beb17c1a9b1cdac
Pavel <pavelmth@gmail.com>
2019-09-07 16:41:32 +0300

    half

--- src/main/java/com/geekbrains/hibernate/myhomework/MainClass.java
+++ src/main/java/com/geekbrains/hibernate/myhomework/MainClass.java
@@ -6,6 +6,7 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import javax.persistence.LockModeType;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -41,97 +42,130 @@ public class MainClass {
 
         Session session = null;
 
-        listOfCommands();
-
         try {
 
             Scanner scanner = new Scanner(System.in);
 
-            //add customer
-            if (scanner.nextLine().equals("ac")) {
-                System.out.println("Input name");
-                session = factory.getCurrentSession();
-                session.beginTransaction();
-                String name = scanner.nextLine();
-                Customer customer = new Customer();
-                customer.setName(name);
-                if(name!= null){
-                    session.save(customer);
-                    System.out.println("Customer " + name + " was added");
-                }
-                session.getTransaction().commit();
-            }
+            String command = "";
 
-            //add product
-            if (scanner.nextLine().equals("ap")) {
-                System.out.println("Input name");
-                String name = scanner.nextLine();
-                System.out.println("Input cost");
-                Integer cost = scanner.nextInt();
-                Product product = new Product();
-                product.setName(name);
-                product.setCost(cost);
-                if(name!= null){
+            while (!command.equals("q")) {
+                listOfCommands();
+                command = scanner.nextLine();
+
+                //add customer
+                if (command.equals("ac")) {
+                    System.out.println("Input name");
                     session = factory.getCurrentSession();
                     session.beginTransaction();
-                    session.save(product);
-                    System.out.println("Product " + name + " and cost " + cost + " was added");
+                    String name = scanner.nextLine();
+                    Customer customer = new Customer();
+                    customer.setName(name);
+                    if(name!= null){
+                        session.save(customer);
+                        System.out.println("Customer " + name + " was added");
+                    }
+                    session.getTransaction().commit();
                 }
-                session.getTransaction().commit();
-            }
 
-            //show list of customers
-            if (scanner.nextLine().equals("sc")) {
-                session = factory.getCurrentSession();
-                session.beginTransaction();
-                Query query = session.createNativeQuery("SELECT * FROM customers;", Customer.class);
-                List<Customer> customers = query.getResultList();
-                session.getTransaction().commit();
-                for (Customer o:
-                        customers) {
-                    System.out.println(o.getName());
+                //add product
+                if (command.equals("ap")) {
+                    System.out.println("Input name");
+                    String name = scanner.nextLine();
+                    System.out.println("Input cost");
+                    Integer cost = scanner.nextInt();
+                    Product product = new Product();
+                    product.setName(name);
+                    product.setCost(cost);
+                    if(name!= null){
+                        session = factory.getCurrentSession();
+                        session.beginTransaction();
+                        session.save(product);
+                        System.out.println("Product " + name + " and cost " + cost + " was added");
+                    }
+                    session.getTransaction().commit();
                 }
-            }
 
-            //show list of products
-            if (scanner.nextLine().equals("sp")) {
-
-            }
-
-            //delete customer
-            if (scanner.nextLine().equals("dc")) {
-                int a = scanner.nextInt();
-                session = factory.getCurrentSession();
-                session.beginTransaction();
-                Customer customer = session.get(Customer.class, a);
-                if(customer!=null){
-                    session.delete(customer);
-                    System.out.println("Customer " + a + " was deleted");
+                //show list of customers
+                if (command.equals("sc")) {
+                    session = factory.getCurrentSession();
+                    session.beginTransaction();
+                    Query query = session.createNativeQuery("SELECT * FROM customers;", Customer.class);
+                    List<Customer> customers = query.getResultList();
+                    for (Customer o:
+                            customers) {
+                        System.out.println(o.getName());
+                    }
+                    session.getTransaction().commit();
                 }
-                session.getTransaction().commit();
-            }
 
-            //delete product
-            if (scanner.nextLine().equals("dp")) {
-                int a = scanner.nextInt();
-                session = factory.getCurrentSession();
-                session.beginTransaction();
-                Product product = session.get(Product.class, a);
-                if(product!=null){
-                    session.delete(product);
-                    System.out.println("Product " + a + " was deleted");
+                //show list of products
+                if (command.equals("sp")) {
+                    session = factory.getCurrentSession();
+                    session.beginTransaction();
+                    Query query = session.createNativeQuery("SELECT * FROM products;", Product.class);
+                    List<Product> products = query.getResultList();
+
+                    for (Product o:
+                            products) {
+                        System.out.println(o.getName());
+                    }
+                    session.getTransaction().commit();
                 }
-                session.getTransaction().commit();
-            }
 
-            //show customer purchases
-            if (scanner.nextLine().equals("scp")) {
+                //delete customer
+                if (command.equals("dc")) {
+                    String name = scanner.nextLine();
+                    session = factory.getCurrentSession();
+                    session.beginTransaction();
+                    Customer customer = session.createQuery("SELECT c FROM Customer c WHERE c.name = :name", Customer.class)
+                            .setParameter("name", name)
+                            .getSingleResult();
+                    if(customer!=null){
+                        session.delete(customer);
+                        System.out.println("Customer " + name + " was deleted");
+                    }
+                    session.getTransaction().commit();
+                }
 
-            }
+                //delete product
+                if (command.equals("dp")) {
+                    String name = scanner.nextLine();
+                    session = factory.getCurrentSession();
+                    session.beginTransaction();
+                    Product product = session.createQuery("SELECT p FROM Product p WHERE p.name = :name", Product.class)
+                            .setParameter("name", name)
+                            .getSingleResult();
+                    if(product!=null){
+                        session.delete(product);
+                        System.out.println("Customer " + name + " was deleted");
+                    }
+                    session.getTransaction().commit();
+                }
 
-            //show customers who bought certain product
-            if (scanner.nextLine().equals("scbp")) {
+                //show customer purchases
+                if (command.equals("scp")) {
+                    String name = scanner.nextLine();
+                    List<Product> products = new ArrayList<>();
+                    session = factory.getCurrentSession();
+                    session.beginTransaction();
+                    Customer customer = session.createQuery("SELECT c FROM Customer c WHERE c.name = :name", Customer.class)
+                            .setParameter("name", name)
+                            .getSingleResult();
+                    if(customer!=null){
+                        for (Basket o :
+                             customer.getBasketList()) {
+                            products.add(session.createQuery("SELECT p FROM Product p WHERE p.customer_id = :id", Product.class)
+                                    .setParameter("id", o.getProductId())
+                                    .getSingleResult());
+                        }
+                    }
+                    session.getTransaction().commit();
+                }
 
+                //show customers who bought certain product
+                if (command.equals("scbp")) {
+
+                }
             }
 
             factory.close();
